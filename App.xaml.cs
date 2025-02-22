98179086f79f7482156229c5aba582e3ab081f9f
zonex <zonex@>
2025-02-22 22:14:52 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Base64Encoder
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace Base64Encoder
+{
+    public partial class MainWindow
+    {
+        private bool _textEncoded;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _textEncoded = !_textEncoded;
+            if (_textEncoded)
+            {
+                TbText.Text = ConvertToBase64(TbText.Text);
+                BtEncode.Content = "Base64";
+            }
+            else
+            {
+                var text = TbText.Text;
+                TbText.Text = ConvertFromBase64(text);
+                BtEncode.Content = "ASCII";
+            }
+        }
+
+        static string ConvertToBase64(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return string.Empty;
+            var bytes = Encoding.UTF8.GetBytes(text);
+            return Convert.ToBase64String(bytes);
+        }
+
+        static string ConvertFromBase64(string base64)
+        {
+            if (string.IsNullOrWhiteSpace(base64)) return string.Empty;
+            var bytes = Convert.FromBase64String(base64);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.SaveFileDialog
+            {
+                DefaultExt = ".txt",
+                Filter = "Text documents (.txt)|*.txt"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, TbText.Text);
+            }
+        }
+
+        private void ButtonCopy_OnClick(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(TbText.Text);
+        }
+    }
+}
